484f1dd4d4a7c1a76b5fe2275f06f5d4e4a16f12
Simon Rei <me@dieideeistgut.de>
2018-10-05 11:56:23 +0200

    init

---
+++ config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+// place overrides here
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 20
+#define RGBLIGHT_HUE_STEP 16
+#define RGBLIGHT_SAT_STEP 16
+#define RGBLIGHT_VAL_STEP 16
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_EFFECT_KNIGHT_OFFSET 8
+
+#define BACKLIGHT_BREATHING
+#define BREATHING_PERIOD 6
+
+#endif
